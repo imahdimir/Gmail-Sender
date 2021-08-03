9636d3c9bbd1d3cbdbeff1297349302212ac510a
Mahdi Mir <imahdimir@gmail.com>
2021-08-03 11:31:43 +0430

    Refinement

--- GmailSender.py
+++ GmailSender.py
@@ -7,24 +7,25 @@ from glob import glob
 from pathlib import Path
 import pandas as pd
 
-
 if getattr(sys, 'frozen', False):
     cwd = Path(sys.executable).parent
 elif __file__:
     cwd = Path(__file__).parent
 
+def write_mail(sender, subj, index_mail, data_row):
+    r = data_row
 
-def write_mail(sender, subj, df_row):
-    r = df_row
     msg = MIMEMultipart()
+
     msg['From'] = sender
-    msg['To'] = r.Email
+    msg['To'] = index_mail
     msg['Subject'] = subj  # The subject line
-    mail_cont = ''.join(str(col) + ':  ' + str(r[col]) + '\n' for col in
-                        r.index.difference(['Email']))
+
+    mail_cont = ''.join(
+            str(col) + ':  ' + str(r[col]) + '\n' for col in r.index)
+
     msg.attach(MIMEText(mail_cont, 'plain'))
     return msg.as_string()
-
 
 def main():
     pass
@@ -38,25 +39,27 @@ def main():
 
     xlpn = glob(f'{cwd}/*.xlsx')
     xlpn = [x for x in xlpn if x[:2] != '~$']
-    if len(xlpn) != 1:
-        print('None or More than one Excel file')
-        return None
+    assert len(xlpn) == 1, 'None or More than one Excel file'
 
     data = pd.read_excel(xlpn[0])
     data = data[data['Email'].notna()]
+    data = data.set_index('Email')
     print(data)
-
+    ##
     session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
     session.starttls()  # enable security
     session.login(sender_address, sender_pass)
 
     for ind, row in data.iterrows():
-        text = write_mail(sender=sender_address, subj=subject, df_row=row)
-        session.sendmail(sender_address, row.Email, text)
-        print(f'Mail Sent To {row.Email}')
+        text = write_mail(sender = sender_address,
+                          subj = subject,
+                          index_mail = ind,
+                          data_row = row)
+        session.sendmail(sender_address, ind, text)
+        print(text)
+        print(f'Mail Sent To {ind}')
 
     session.quit()
-
 
 ##
 if __name__ == '__main__':
